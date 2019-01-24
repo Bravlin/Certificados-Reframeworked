039debf0069e5ab683ac3bcad8fab42477991142
Bravlin <bravlinleonel@gmail.com>
2019-01-24 18:55:18 -0300

    Importados recursos generales y creado layout.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('head-particular')
+    <link rel="stylesheet" type="text/css" href="/css/item-consulta.css">
+@endsection
+
+@section('contenido')
+    <h1 class="mb-5 text-center">Bienvenido al sistema de envío de certificados</h1>
+    <h3 class="mb-4">Eventos agregados recientemente</h3>
+@endsection
