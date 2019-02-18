a302bf4d64d7787bd50626f9686a62e505df5ab8
Bravlin <bravlinleonel@gmail.com>
2019-02-18 16:57:53 -0300

    Problema anterior era por el log de Laravel. Implementado envío masivo.

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Evento;
 use App\Inscripcion;
 use Illuminate\Http\Request;
 use App\Mail\EnvioCertificado;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class MailController extends Controller
 {
@@ -20,19 +22,36 @@ class MailController extends Controller
         ]);
     }
 
-    public function envioCertificadoIndividual(Request $request, Inscripcion $inscripcion)
+    private function enviarCertificado($campos, Inscripcion $inscripcion)
     {
-        $campos = $this->validarEnvioCertificado($request);
         $certificado = $inscripcion->certificado;
         if (!Storage::disk('public')->exists('certificados/'.$certificado->nombre_certificado)) {
             Log::error('Certificado correspondiente a inscripción #'.$inscripcion->id_inscripcion.' no encontrado.');
-            abort(500);
+            throw new HttpException(500);
         } else {
             $destinatario = $inscripcion->perfil->email;
             Mail::to($destinatario)
                 ->queue(new EnvioCertificado($campos['remitente'], $campos['asunto'], $campos['cuerpo_mail'], $certificado->nombre_certificado));
             $certificado->email_enviado = $destinatario;
             $certificado->save();
+        }
+    }
+
+    public function envioCertificadoIndividual(Request $request, Inscripcion $inscripcion)
+    {
+        $campos = $this->validarEnvioCertificado($request);
+        $this->enviarCertificado($campos, $inscripcion);
+    }
+
+    public function envioCertificadosMasivo(Request $request, Evento $evento)
+    {
+        $campos = $this->validarEnvioCertificado($request);
+        foreach ($evento->inscripcionesAsistentes() as $inscripcion) {
+            try {
+                $this->enviarCertificado($campos, $inscripcion);
+            } catch (HttpException $e) {
+                Log::info('Envío de certificado correspondiente a inscripción #'.$inscripcion->id_inscripcion.' omitido.');
+            }
         }
     }
 }
