698e2eebc538bcdc8aae3763ef6928e2d2139d22
Bravlin <bravlinleonel@gmail.com>
2019-03-30 18:23:35 -0300

    Script de inicializacion.
    El script genera la estructura de directorios necesaria para la aplicacion. Los archivos por defecto ahora han sido movidos de lugar y trackeados y la aplicacion refleja este cambio.

---
+++ setup-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RUTA_SCRIPT=$(dirname `which $0`)
+STORAGE_PUBLIC="$RUTA_SCRIPT/storage/app/public"
+
+# Crea la estructura de directorios necesaria del storage
+if [ ! -d "$STORAGE_PUBLIC/certificados" ]; then
+    mkdir "$STORAGE_PUBLIC/certificados"
+fi
+if [ ! -d "$STORAGE_PUBLIC/media" ]; then
+    mkdir "$STORAGE_PUBLIC/media"
+    mkdir "$STORAGE_PUBLIC/media/perfiles-usuarios"
+    mkdir "$STORAGE_PUBLIC/media/portadas-eventos"
+fi
+if [ ! -d "$STORAGE_PUBLIC/templates" ]; then
+    mkdir "$STORAGE_PUBLIC/templates"
+fi
+if [ ! -d "$STORAGE_PUBLIC/tmp" ]; then
+    mkdir "$STORAGE_PUBLIC/tmp"
+fi
+
+# Crea un enlace simbólico al storage publico para acceder desde public
+php "$RUTA_SCRIPT/artisan" storage:link
